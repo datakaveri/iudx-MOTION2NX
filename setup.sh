7437bcc02dbeb2a07126c9944f210c6657382baf
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2023-04-08 20:32:33 +0530

    simplify setup.sh using pwd commaqnd

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 variable_name=BASE_DIR
-variable_value=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+variable_value=`pwd`
 echo "export "$variable_name"="$variable_value>>~/.bashrc
 echo $variable_name"="$variable_value>>~/.profile
 source ~/.bashrc
