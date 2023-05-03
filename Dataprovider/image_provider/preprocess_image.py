8e362bc58c1b42560cdd4920a59fe3fb63cbbe14
guptaashreyas <guptaashreyas@LAPTOP-JG2FHMSB>
2023-05-03 16:33:37 +0530

    Added a file to preprocess images of handwritten numbers to get them to MNIST standard

--- Dataprovider/image_provider/preprocess_image.py
+++ Dataprovider/image_provider/preprocess_image.py
@@ -23,14 +23,14 @@ def shift(img,sx,sy):
 def main():
     base_dir = os.getenv("BASE_DIR")
 
-    img_dir = os.path.join(base_dir,"example-data/data/ImageProvider")
+    img_dir = os.path.join(base_dir,"data/ImageProvider")
 
     # print(img_dir)
 
     j = str(9)
 
     # read the image
-    for i in range (0,200):
+    for i in range (0, 1):
         # old_img = Image.open(os.path.join(img_dir,"raw_images/9/"+str(i)+".png"))
 
         # img = Image.new("RGBA", old_img.size, "WHITE")
@@ -38,59 +38,65 @@ def main():
         # img.convert("RGB").save(os.path.join(img_dir,"raw_images/9/"+str(i)+".png"))
 
         # img.show()
+
+        # path = os.path.join(img_dir,"raw_images/"+j+"/"+str(i)+".png")
         
-        img = cv2.imread(os.path.join(img_dir,"raw_images/"+j+"/"+str(i)+".png"), cv2.IMREAD_GRAYSCALE)
+        # img_ = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+        img_ = cv2.imread(os.path.join(img_dir,"raw_images/2222.png"), cv2.IMREAD_GRAYSCALE)
 
         # img = cv2.imread(no, 0)
         
         # rescale it
-        img = cv2.resize(255-img, (28, 28))
+        img_ = cv2.resize(255-img_, (28, 28))
 
         # better black and white version
-        (thresh, img) = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        (thresh, img_) = cv2.threshold(img_, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-        while np.sum(img[0]) == 0:
-            img = img[1:]
+        while np.sum(img_[0]) == 0:
+            img_ = img_[1:]
 
-        while np.sum(img[:,0]) == 0:
-            img = np.delete(img,0,1)
+        while np.sum(img_[:,0]) == 0:
+            img_ = np.delete(img_,0,1)
 
-        while np.sum(img[-1]) == 0:
-            img = img[:-1]
+        while np.sum(img_[-1]) == 0:
+            img_ = img_[:-1]
         
-        while np.sum(img[:,-1]) == 0:
-            img = np.delete(img,-1,1)
+        while np.sum(img_[:,-1]) == 0:
+            img_ = np.delete(img_,-1,1)
 
-        rows, cols = img.shape
+        rows, cols = img_.shape
 
         if rows > cols:
             factor = 20.0/rows
             rows = 20
             cols = int(round(cols*factor))
 	        # first cols than rows
-            img = cv2.resize(img, (cols,rows))
+            img_ = cv2.resize(img_, (cols,rows))
     
         else:
             factor = 20.0/cols
             cols = 20
             rows = int(round(rows*factor))
     	    # first cols than rows
-            img = cv2.resize(img, (cols, rows))
+            img_ = cv2.resize(img_, (cols, rows))
     
         colsPadding = (int(math.ceil((28-cols)/2.0)),int(math.floor((28-cols)/2.0)))
         rowsPadding = (int(math.ceil((28-rows)/2.0)),int(math.floor((28-rows)/2.0)))
-        img = np.lib.pad(img,(rowsPadding,colsPadding),'constant')
+        img_ = np.lib.pad(img_,(rowsPadding,colsPadding),'constant')
 
         # shifitng the image to center it at the centre of gravity of the number
-        shiftx,shifty = getBestShift(img)
-        shifted = shift(img,shiftx,shifty)
-        img = shifted
+        shiftx,shifty = getBestShift(img_)
+        shifted = shift(img_,shiftx,shifty)
+        img_ = shifted
 
         # save the processed images
-        if not(os.path.exists(os.path.join(img_dir,"processed_images/"+j+"/"))):
-            os.mkdir(os.path.join(img_dir,"processed_images/"+j+"/"))
+        # if not(os.path.exists(os.path.join(img_dir,"processed_images/"+j+"/"))):
+        #     os.mkdir(os.path.join(img_dir,"processed_images/"+j+"/"))
         
-        cv2.imwrite(os.path.join(img_dir,"processed_images/"+j+"/"+str(i)+".png"), img)
+        # cv2.imwrite(os.path.join(img_dir,"processed_images/"+str(i)+".png"), img_)
+        cv2.imwrite(os.path.join(img_dir,"processed_images/2222.png"), img_)
+
 
         """
         all images in the training set have an range from 0-1
@@ -98,12 +104,12 @@ def main():
         (a one dimensional vector with our 784 pixels)
         to use the same 0-1 based range
         """
-        flattened_img = img.flatten() / 255.0
+        flattened_img = img_.flatten() / 255.0
         new_number = int(j)
 
         # Insert the new number at the top of the array
         final_image = np.insert(flattened_img, 0, new_number, axis=0)
-        np.savetxt(os.path.join(img_dir,"images/"+str(19000+i)+".csv"),final_image,delimiter=",")
+        np.savetxt(os.path.join(img_dir,"images/X2222.csv"),final_image,delimiter=",")
 
     return 1
 
