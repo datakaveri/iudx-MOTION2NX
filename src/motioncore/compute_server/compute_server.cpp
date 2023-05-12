d7dc3f2781d4aed3553a3eeb50999fcf1cec334e
srishti <srishti.mittal@datakaveri.org>
2023-05-12 15:05:05 +0530

    add/modify src files to run generalised version

--- src/motioncore/compute_server/compute_server.cpp
+++ src/motioncore/compute_server/compute_server.cpp
@@ -96,6 +96,35 @@ std::vector<Shares> read_struct(tcp::socket& socket, int num_elements) {
   return data;
 }
 
+/////////////////////////////////////////////////////////////////////
+std::vector<Shares> read_struct_vector(tcp::socket& socket, int num_elements) {
+  std::cout << "Before reading of data\n";
+  std::vector<Shares> data;
+  boost::system::error_code ec;
+  uint64_t arr[2 * num_elements];
+  read(socket, boost::asio::buffer(&arr, sizeof(arr)), ec);
+  if (ec) {
+    std::cout << "Error:" << ec.message() << "\n";
+  } else {
+    cout << "Received successfully \n";
+  }
+  int j = 0;
+  cout << "Num elemen\n";
+  for (int i = 0; i < num_elements; i++) {
+    Shares temp;
+    temp.Delta = arr[j];
+    // std::cout << temp.Delta << " ";
+    j = j + 1;
+    temp.delta = arr[j];
+    j = j + 1;
+    // std::cout << temp.delta << "\n";
+    data.push_back(temp);
+  }
+
+  // socket.close();
+  return data;
+}
+
 std::pair<std::vector<Shares>, int> get_provider_dot_product_data(int port_number) {
   boost::asio::io_service io_service;
 
@@ -378,6 +407,91 @@ get_provider_mat_mul_data_new(int port_number) {
   return {actualanswer, fractional_bits, std::make_pair(data, dims)};
 }
 
+//////////////////Function to receive all vectors one shot, Ramya may 3,2023
+std::tuple<int, std::size_t,
+           std::pair<std::vector<std::vector<Shares>>, std::vector<std::vector<int>>>>
+get_provider_total_data(int port_number) {
+  cout << "Inside new function \n";
+  boost::asio::io_service io_service;
+
+  // listen for new connection
+  tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), port_number));
+
+  // socket creation
+  tcp::socket socket_(io_service);
+
+  // waiting for the connection
+  acceptor_.accept(socket_);
+  boost::system::error_code ec;
+  size_t fractional_bits;
+  int numberOfVectors;
+  const string msg_layer = "Server has received the number of vectors expected \n";
+  const string msg_init = "Server has received the number of fractional bits \n";
+  int arr1[2], arr2[2], arr3[2], arr4[2];
+  const string msg = "Server has received the number of elements \n";
+  /////////Receiving 2*number of layers or number of vectors to be expected
+  read(socket_, boost::asio::buffer(&numberOfVectors, sizeof(numberOfVectors)), ec);
+  if (ec) {
+    cout << ec << "\n";
+  } else {
+    cout << "No Error\n";
+  }
+
+  boost::asio::write(socket_, boost::asio::buffer(msg_layer));
+  cout << "Server received number of vectors!" << endl;
+
+  ///////////
+
+  // Read and write the number of fractional bits
+  read(socket_, boost::asio::buffer(&fractional_bits, sizeof(fractional_bits)), ec);
+  if (ec) {
+    cout << ec << "\n";
+  } else {
+    cout << "No Error\n";
+  }
+
+  boost::asio::write(socket_, boost::asio::buffer(msg_init));
+  cout << "Server sent ack message to Client!" << endl;
+  std::vector<std::vector<int>> dims;
+  int num_elements1;
+  std::vector<std::vector<Shares>> data;
+  const string msgw1 = "Server has received w1 \n";
+  //////////////////////////////// Repeatedly use the following////////////////
+  // read operation
+  for (int i = 0; i < 4; i++) {
+    read(socket_, boost::asio::buffer(&arr1, sizeof(arr1)), ec);
+    if (ec) {
+      cout << ec << "\n";
+    } else {
+      cout << "No Error\n";
+    }
+    std::cout << "Before dims1 push back \n";
+    dims.push_back({arr1[0], arr1[1]});
+    // dims1.push_back(arr1[1]);
+    std::cout << "After dims1 push back \n";
+    num_elements1 = arr1[0] * arr1[1];
+    cout << "Rows, columns" << arr1[0] << " " << arr1[1] << endl;
+    cout << "The number of elements are :- " << num_elements1 << endl;
+
+    // write operation
+    boost::asio::write(socket_, boost::asio::buffer(msg));
+    cout << "Server sent message to Client!" << endl;
+
+    // Read the data in the required format
+    cout << "Before read struct vector " << endl;
+    data.push_back(read_struct_vector(socket_, num_elements1));
+    boost::asio::write(socket_, boost::asio::buffer(msgw1));
+    // cout << "Servent sent w1 to Client!" << endl;
+    std::cout << "Finished reading input W1\n\n";
+  }
+  socket_.close();
+  std::cout << "Dims 1:" << dims[0][0] << " " << dims[0][1] << endl;
+  std::cout << "Dims 2:" << dims[1][0] << " " << dims[1][1] << endl;
+  std::cout << "Dims 3:" << dims[2][0] << " " << dims[2][1] << endl;
+  std::cout << "Dims 4:" << dims[3][0] << " " << dims[3][1] << endl;
+  return {numberOfVectors, fractional_bits, std::make_pair(data, dims)};
+}
+
 std::pair<std::size_t, std::pair<std::vector<Shares>, std::vector<int>>>
 get_provider_mat_mul_const_data(int port_number) {
   boost::asio::io_service io_service;
@@ -450,5 +564,3 @@ get_provider_mat_mul_const_data(int port_number) {
 }
 
 }  // namespace COMPUTE_SERVER
-
-
--- src/motioncore/compute_server/compute_server.h
+++ src/motioncore/compute_server/compute_server.h
@@ -21,4 +21,7 @@ std::tuple<int, std::size_t, std::pair<std::vector<Shares>, std::vector<int>>>
 get_provider_mat_mul_data_new(int port_number);
 std::pair<std::size_t, std::pair<std::vector<Shares>, std::vector<int>>>
 get_provider_mat_mul_const_data(int port_number);
+std::tuple<int, std::size_t,
+           std::pair<std::vector<std::vector<Shares>>, std::vector<std::vector<int>>>>
+get_provider_total_data(int port_number);
 }  // namespace COMPUTE_SERVER
