275aac06cfc5c9afc6d96e79f4c83a795f23e035
guptaashreyas <guptaashreyas@LAPTOP-JG2FHMSB>
2023-05-22 12:41:56 +0530

    Files added to generalise sending and receiving of weight and bias shares, along with the config file

---
+++ Dataprovider/weights_provider/weights_provider_genr.cpp
@@ -0,0 +1,468 @@
+//  ./bin/weights_provider_genr --compute-server0-ip 127.0.0.1 --compute-server0-port 1234
+//  --compute-server1-ip 127.0.0.1 --compute-server1-port 1235 --dp-id 0 --fractional-bits 13
+//  --filepath ${BASE_DIR}/data/ModelProvider --config-file-path ${BASE_DIR}/config_files/model_config.json
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
+#include <boost/serialization/string.hpp>
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <random>
+#include <stdexcept>
+#include <type_traits>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+using namespace std::chrono;
+namespace pt = boost::property_tree;
+
+#include "./fixed-point.h"
+
+using namespace boost::asio;
+namespace po = boost::program_options;
+namespace fs = std::filesystem; 
+using ip::tcp;
+using std::cout;
+using std::endl;
+using std::string;
+
+struct Options {
+  std::string cs0_ip;
+  int cs0_port;
+  std::string cs1_ip;
+  int cs1_port;
+  std::size_t fractional_bits;
+  std::string fullfilepath;
+  std::filesystem::path config_file;
+  int numberOfLayers;
+};
+
+std::optional<Options> parse_program_options(int argc, char* argv[]) {
+  Options options;
+  boost::program_options::options_description desc("Allowed options");
+  // clang-format off
+  desc.add_options()
+    ("help,h", po::bool_switch()->default_value(false),"produce help message")
+    ("compute-server0-ip", po::value<std::string>()->default_value("127.0.0.1"), "IP address of compute server 0")
+    ("compute-server0-port", po::value<int>()->required(), "Port number of compute server 0")
+    ("compute-server1-ip", po::value<std::string>()->default_value("127.0.0.1"), "IP address of compute server 1")
+    ("compute-server1-port", po::value<int>()->required(), "Port number of compute server 1")
+    ("fractional-bits", po::value<size_t>()->required(), "Number of fractional bits")
+    ("dp-id", po::value<int>()->required(), "Id of the data provider")
+    ("filepath", po::value<string>()->required(), "Directory where the weights and biases are stored")
+    ("config-file-path", po::value<string>()->required(), "File path which has the name of all the weights and bias csv files")
+    ;
+  // clang-format on
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  bool help = vm["help"].as<bool>();
+  if (help) {
+    std::cerr << desc << "\n";
+    return std::nullopt;
+  }
+
+  options.cs0_ip = vm["compute-server0-ip"].as<std::string>();
+  options.cs0_port = vm["compute-server0-port"].as<int>();
+
+  options.cs1_ip = vm["compute-server1-ip"].as<std::string>();
+  options.cs1_port = vm["compute-server1-port"].as<int>();
+
+  options.fractional_bits = vm["fractional-bits"].as<size_t>();
+  options.fullfilepath = vm["filepath"].as<std::string>();
+  options.config_file = vm["config-file-path"].as<std::string>();
+
+  return options;
+}
+
+template <typename E>
+std::uint64_t blah(E& engine) {
+  std::uniform_int_distribution<unsigned long long> dis(std::numeric_limits<std::uint64_t>::min(),
+                                                        std::numeric_limits<std::uint64_t>::max());
+  return dis(engine);
+}
+
+struct Shares {
+  uint64_t Delta, delta;
+};
+
+///////////////////////////////////////////////////////////////
+void write_struct_vector(tcp::socket& socket, Shares* data, int num_elements) {
+  std::cout << "Inside write struct \n";
+  uint64_t arr[2 * num_elements];
+  int j = 0;
+  for (int i = 0; i < num_elements; i++) {
+    // uint64_t arr[2];
+    arr[j] = data[i].Delta;
+    j++;
+    arr[j] = data[i].delta;
+    j++;
+  }
+  boost::system::error_code error;
+  //  cout << "Start write operation " << endl;
+  boost::asio::write(socket, boost::asio::buffer(&arr, sizeof(arr)), error);
+
+  if (error) {
+    cout << "Send failed: " << error.message() << endl;
+  } else {
+    cout << "Sent successfully \n";
+  }
+}
+
+///////////////////////////////////////////////////////////////////
+void write_struct(tcp::socket& socket, Shares* data, int num_elements) {
+  for (int i = 0; i < num_elements; i++) {
+    uint64_t arr[2];
+    arr[0] = data[i].Delta;
+    arr[1] = data[i].delta;
+
+    boost::system::error_code error;
+    boost::asio::write(socket, boost::asio::buffer(&arr, sizeof(arr)), error);
+
+    if (!error) {
+      continue;
+    } else {
+      cout << "send failed: " << error.message() << endl;
+    }
+  }
+}
+
+class Matrix {
+ private:
+  uint64_t rows, columns, num_elements, fractional_bits;
+  std::string fullFileName;
+  std::vector<float> data;
+  struct Shares *cs0_data, *cs1_data;
+
+ public:
+  Matrix(int row, int col, std::string fileName, std::size_t fraction_bits,
+         const Options& options) {
+    rows = row;
+    columns = col;
+    num_elements = row * col;
+    fractional_bits = fraction_bits;
+
+    fullFileName = fileName;
+    std::cout << fullFileName << std::endl;
+    cs0_data = (struct Shares*)malloc(sizeof(struct Shares) * num_elements);
+    cs1_data = (struct Shares*)malloc(sizeof(struct Shares) * num_elements);
+  }
+  /////////Function to access rows and columns of a Matrix, Ramya May 3,2023
+  std::pair<int, int> getShareDimensions() {
+    std::pair<int, int> dimensions;
+    dimensions.first = rows;
+    dimensions.second = columns;
+    return dimensions;
+  }
+
+  int getNumberofElements() { return num_elements; }
+  string getFileName() { return fullFileName; }
+  /////////Function to aceess data from the Matrix, Ramya May 3,2023
+  Shares* getData(int a) {
+    if (a == 0) {
+      return cs0_data;
+    } else {
+      return cs1_data;
+    }
+  }
+
+  void readMatrixCSV() {
+    auto start = high_resolution_clock::now();
+
+    std::cout << "Reading the matrix from file :- " << fullFileName << std::endl;
+
+    std::ifstream indata;
+    indata.open(fullFileName);
+
+    std::string line;
+    while (std::getline(indata, line)) {
+      std::stringstream lineStream(line);
+      std::string cell;
+
+      while (std::getline(lineStream, cell, ',')) {
+        data.push_back(stold(cell));
+      }
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Duration for reading from the file:" << duration.count() << "msec" << endl;
+  }
+
+  void printData() {
+    std::cout << "Printing data\n";
+
+    for (int i = 0; i < data.size(); i++) {
+      std::cout << data[i] << " ";
+    }
+
+    std::cout << "\n";
+  }
+
+  void generateShares() {
+    // Now that we have data, need to generate the shares
+    for (int i = 0; i < data.size(); i++) {
+      std::random_device rd;
+      std::mt19937 gen(rd());
+      std::uint64_t del0 = blah(gen);
+      std::uint64_t del1 = blah(gen);
+      std::uint64_t Del =
+          del0 + del1 +
+          MOTION::new_fixed_point::encode<uint64_t, long double>(data[i], fractional_bits);
+      // For each data, creating 2 shares variables - 1 for CS0 and another for CS1
+      Shares cs0, cs1;
+
+      cs0.Delta = Del;
+      cs0.delta = del0;
+
+      cs1.Delta = Del;
+      cs1.delta = del1;
+
+      cs0_data[i] = cs0;
+      cs1_data[i] = cs1;
+
+      // std::cout << "Data = " << data[i] << " Delta = " << cs0.Delta << " delta0 = " << cs0.delta
+      // << " delta1 = " << cs1.delta << "\n";
+    }
+
+    return;
+  }
+};
+
+std::vector<Matrix> read_Data(Options& options) {
+  std::ifstream jsonFile(options.config_file);
+
+  std::vector<Matrix> allData;
+  fs::path data_dir(options.fullfilepath);
+
+  if (jsonFile.is_open()){   //checking whether the file is open
+      pt::ptree pt;
+      pt::read_json(jsonFile, pt);
+      
+      string data_file_name;
+      int num_layers, num_rows, num_columns;
+
+      fs::path data_file_path;
+
+      int curr_layer_id = 0;
+
+      for (auto &json_elem : pt) {  //read data from file object and put it into string.
+        if (json_elem.first == "no_of_layers") {
+          options.numberOfLayers = stoi(json_elem.second.data());
+        }
+        else {
+          for (auto &layer_id : json_elem.second) {
+            curr_layer_id++;
+            for (auto &data : layer_id.second) {
+              
+              for(auto &property : data.second) {
+                
+                if (property.first == "rows") {
+                  num_rows = stoi(property.second.data());
+                }
+                else if (property.first == "columns") {
+                  num_columns = stoi(property.second.data());
+                }
+                else {
+                  std::string file_name = property.second.data();
+                  fs::path data_file_name(file_name);
+                  data_file_path = data_dir/data_file_name;
+                }
+              }
+
+              // std::cout << curr_layer_id << " " << options.numberOfLayers << " " << num_rows << " " << num_columns << " " << data_file_path << " " << std::endl;
+              
+              Matrix vector_data = Matrix(num_rows, num_columns, data_file_path, options.fractional_bits, options);
+              vector_data.readMatrixCSV();
+              vector_data.generateShares();
+              allData.push_back(vector_data);
+            }
+            if(curr_layer_id > options.numberOfLayers) {
+              cout << "Error: Number of layers in model config exceed number of layers stated." << std::endl;
+              EXIT_FAILURE;
+            }                  
+          }
+        }
+      }
+      if(curr_layer_id < options.numberOfLayers) {
+        cout << "Error: Details of the layers provided are leser than number of layers. Please provide all the details of all the layers." << std::endl;
+        EXIT_FAILURE; 
+      }
+      jsonFile.close();   //close the file object.
+  }
+  else {
+    std::cout << "Error in opening file" << std::endl;
+    EXIT_FAILURE;
+  }
+  return allData;
+}
+
+void send_shares_to_servers(std::vector<Matrix> data_shares, const Options& options) {
+  std::cout << "Sending shares to the compute servers." << std::endl;
+
+  for(int i = 0; i < 2; ++i) {
+    boost::asio::io_service io_service;
+
+    // socket creation
+    tcp::socket socket(io_service);
+
+    uint64_t frac_bits = options.fractional_bits;
+    std::cout << "Frac bits:" << options.fractional_bits << "\n";
+    auto port = options.cs0_port;
+    auto ip = options.cs0_ip;
+    if (i) {
+      port = options.cs1_port;
+      ip = options.cs1_ip;
+    }
+
+    // connection
+    socket.connect(tcp::endpoint(boost::asio::ip::address::from_string(ip), port));
+
+    /////////Sending number of layers to be expected
+    // int numberOfLayers = 5;
+    // int numberOfVectors = 2 * numberLayers;
+    boost::system::error_code error_layer;
+    boost::asio::write(socket, boost::asio::buffer(&options.numberOfLayers, sizeof(options.numberOfLayers)),
+                       error_layer);
+    if (!error_layer) {
+      cout << "Not an error" << endl;
+    } else {
+      cout << "send failed: " << error_layer.message() << endl;
+    }
+
+    // getting a response from the server
+    boost::asio::streambuf receive_buffer_layer;
+    boost::asio::read_until(socket, receive_buffer_layer, "\n");
+    if (error_layer && error_layer != boost::asio::error::eof) {
+      cout << "receive failed: " << error_layer.message() << endl;
+    } else {
+      const char* data = boost::asio::buffer_cast<const char*>(receive_buffer_layer.data());
+      cout << data << endl;
+    }
+
+    //////////////////////////////////////
+
+    // First send the number of fractional bits to the server
+    boost::system::error_code error_init;
+    boost::asio::write(socket, boost::asio::buffer(&frac_bits, sizeof(frac_bits)), error_init);
+    if (!error_init) {
+      cout << "Not an error" << endl;
+    } else {
+      cout << "send failed: " << error_init.message() << endl;
+    }
+
+    // getting a response from the server
+    boost::asio::streambuf receive_buffer_init;
+    boost::asio::read_until(socket, receive_buffer_init, "\n");
+    if (error_init && error_init != boost::asio::error::eof) {
+      cout << "receive failed: " << error_init.message() << endl;
+    } else {
+      const char* data = boost::asio::buffer_cast<const char*>(receive_buffer_init.data());
+      cout << data << endl;
+    }
+
+    for (auto data_array : data_shares) {
+      std::cout << "File name:" << data_array.getFileName() << "\n";
+
+      // Send rows and columns to compute server
+      int rows, columns;
+      auto vector_dimensions = data_array.getShareDimensions();
+      cout << "Rows, columns: " << vector_dimensions.first << " " << vector_dimensions.second << endl;
+      boost::system::error_code error;
+      boost::asio::write(socket, boost::asio::buffer(&vector_dimensions, sizeof(vector_dimensions)), error);
+      if (!error) {
+        cout << "Not an error" << endl;
+      } else {
+        cout << "send failed: " << error.message() << endl;
+      }
+
+      // getting a response from the server
+      boost::asio::streambuf receive_buffer;
+      boost::asio::read_until(socket, receive_buffer, "\n");
+      if (error && error != boost::asio::error::eof) {
+        cout << "receive failed: " << error.message() << endl;
+      } else {
+        const char* data = boost::asio::buffer_cast<const char*>(receive_buffer.data());
+        cout << data << endl;
+      }
+
+      // Sending CS0 data to compute_server0
+      auto data = data_array.getData(0);
+      if (i) {
+        data = data_array.getData(1);
+      }
+      // std::cout << "inside main size of cs 0 data:" << sizeof(cs0_data)<< "\n";
+      // std::cout << "inside main size of data:" << sizeof(data)<< "\n";
+      //  Use auto keyword to avoid typing long
+      //  type definitions to get the timepoint
+      //  at this instant use function now()
+      auto start = high_resolution_clock::now();
+      int num_of_elements = data_array.getNumberofElements();
+      cout << "Number of elements:" << num_of_elements << endl;
+      cout << "....Sending data...." << "\n";
+      write_struct_vector(socket, data, num_of_elements);
+      cout << "Data sent" << "\n";
+
+      // getting a response from the server
+      boost::asio::streambuf receive_buff;
+      boost::asio::read_until(socket, receive_buff, "\n");
+      if (error && error != boost::asio::error::eof) {
+        cout << "receive failed: " << error.message() << endl;
+      } else {
+        const char* data = boost::asio::buffer_cast<const char*>(receive_buff.data());
+        cout << data << endl;
+      }
+      auto stop = high_resolution_clock::now();
+      auto duration = duration_cast<milliseconds>(stop - start);
+      cout << "Duration to write the vector:" << duration.count() << "msec" << endl;
+    }
+    std::cout << "Data sent to Server " << i << endl;
+    socket.close();
+  }
+}
+
+void send_confirmation(const Options& options) {
+  int validation_bit = 1;
+
+  // for (int i = 0; i < 2; i++) {
+
+  // }
+  // std::cin.ignore();
+  sleep(2);
+  cout << "\nStart of send to compute server\n";
+
+  boost::asio::io_service io_service;
+
+  // socket creation
+  tcp::socket socket(io_service);
+
+  auto port = options.cs0_port;
+  auto ip = options.cs0_ip;
+
+  // connection
+  socket.connect(tcp::endpoint(boost::asio::ip::address::from_string(ip), port));
+  boost::system::error_code error_init;
+  boost::asio::write(socket, boost::asio::buffer(&validation_bit, sizeof(validation_bit)),
+                     error_init);
+
+  socket.close();
+}
+
+int main(int argc, char* argv[]) {
+  auto options = parse_program_options(argc, argv);
+
+  if (!options.has_value()) {
+    return EXIT_FAILURE;
+  }
+  
+  auto model_share_data = read_Data(*options);
+
+  send_shares_to_servers(model_share_data, *options);
+
+  send_confirmation(*options);
+
+  return 0;
+}
---
+++ src/examples/accuracy_testing/weight_share_receiver_genr.cpp
@@ -0,0 +1,231 @@
+/*
+
+./bin/weight_share_receiver_genr --my-id 0 --port 1234 --current-path ${BASE_DIR}/build_debwithrelinfo_gcc
+
+./bin/weight_share_receiver_genr --my-id 1 --port 1235 --current-path ${BASE_DIR}/build_debwithrelinfo_gcc
+
+*/
+/* This code generates shares files for w1,b1,w2,b2.
+
+*/
+
+// MIT License
+//
+// Copyright (c) 2021 Lennart Braun
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <dirent.h>
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <regex>
+#include <stdexcept>
+#include <thread>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/json/serialize.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/program_options.hpp>
+
+#include "algorithm/circuit_loader.h"
+#include "base/gate_factory.h"
+#include "base/two_party_backend.h"
+#include "communication/communication_layer.h"
+#include "communication/tcp_transport.h"
+#include "compute_server/compute_server.h"
+#include "statistics/analysis.h"
+#include "utility/logger.h"
+
+#include "base/two_party_tensor_backend.h"
+#include "protocols/beavy/tensor.h"
+#include "tensor/tensor.h"
+#include "tensor/tensor_op.h"
+#include "tensor/tensor_op_factory.h"
+#include "utility/fixed_point.h"
+
+namespace po = boost::program_options;
+
+static std::vector<uint64_t> generate_inputs(const MOTION::tensor::TensorDimensions dims) {
+  return MOTION::Helpers::RandomVector<uint64_t>(dims.get_data_size());
+}
+
+struct Options {
+  std::size_t threads;
+  bool json;
+  std::size_t num_simd;
+  bool sync_between_setup_and_online;
+  std::size_t my_id;
+  bool no_run = false;
+  std::vector<std::string> data;
+  std::vector<std::string> filepaths;
+  std::string currentpath;
+  int port;
+  int number_of_layers;
+};
+
+std::optional<Options> parse_program_options(int argc, char* argv[]) {
+  Options options;
+  boost::program_options::options_description desc("Allowed options");
+  // clang-format off
+  desc.add_options()
+    ("help,h", po::bool_switch()->default_value(false),"produce help message")
+    ("my-id", po::value<std::size_t>()->required(), "my party id")
+    ("port" , po::value<int>()->required(), "Port number on which to listen")
+    ("threads", po::value<std::size_t>()->default_value(0), "number of threads to use for gate evaluation")
+    ("json", po::bool_switch()->default_value(false), "output data in JSON format")
+    ("num-simd", po::value<std::size_t>()->default_value(1), "number of SIMD values")
+    ("current-path",po::value<std::string>()->required(), "current path build_debwithrelinfo")
+    ("sync-between-setup-and-online", po::bool_switch()->default_value(false),
+     "run a synchronization protocol before the online phase starts")
+    ("no-run", po::bool_switch()->default_value(false), "just build the circuit, but not execute it")
+    ;
+  // clang-format on
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  bool help = vm["help"].as<bool>();
+  if (help) {
+    std::cerr << desc << "\n";
+    return std::nullopt;
+  }
+
+  options.my_id = vm["my-id"].as<std::size_t>();
+  options.port = vm["port"].as<int>();
+  options.threads = vm["threads"].as<std::size_t>();
+  options.json = vm["json"].as<bool>();
+  options.num_simd = vm["num-simd"].as<std::size_t>();
+  options.sync_between_setup_and_online = vm["sync-between-setup-and-online"].as<bool>();
+  options.no_run = vm["no-run"].as<bool>();
+  // options.filenames = vm["file-names"].as<std::string>();
+  options.currentpath = vm["current-path"].as<std::string>();
+  if (options.my_id > 1) {
+    std::cerr << "my-id must be one of 0 and 1\n";
+    return std::nullopt;
+  }
+
+  return options;
+}
+
+void generate_filepaths(Options& options) {
+  std::error_code err;
+  std::string dir_name = "server" + std::to_string(options.my_id);
+  std::string dir_path = options.currentpath + "/" + dir_name;
+  std::filesystem::create_directories(dir_path, err);
+
+  std::string share_data_config_file_name = "file_config_model" + std::to_string(options.my_id);
+  std::string share_data_config_file_path = options.currentpath + "/" + share_data_config_file_name;
+  std::ofstream config_file;
+  config_file.open(share_data_config_file_path, std::ios_base::out);
+
+  for (int i = 1; i <= options.number_of_layers; ++i) {
+    std::string weight_share_file_name, bias_share_file_name;
+    string curr_layer = std::to_string(i);
+
+    weight_share_file_name = "W" + curr_layer;
+    std::string weight_share_file_path = dir_path + "/" + weight_share_file_name;
+    config_file << weight_share_file_path << "\n";
+    
+    bias_share_file_name = "B" + curr_layer;
+    std::string bias_share_file_path = dir_path + "/" + bias_share_file_name;
+    config_file << bias_share_file_path << "\n";
+
+    options.filepaths.push_back(weight_share_file_path);
+    options.filepaths.push_back(bias_share_file_path);
+  }
+}
+
+void retrieve_shares(Options& options) {
+  std::ofstream file;
+
+  ////////////////////////////////////////////////////////////
+
+  auto [numberOfLayers, frac, data_and_dims] =
+      COMPUTE_SERVER::get_provider_total_data(options.port);
+  options.number_of_layers = numberOfLayers;
+
+  generate_filepaths(options);
+
+  for (auto i = 0; i < 2*numberOfLayers; i++) {
+    std::cout << "Writing shares to file\n";
+    auto data_share_file_path = options.filepaths[i];
+    std::cout << data_share_file_path << "\n";
+    std::vector<COMPUTE_SERVER::Shares> input_values_dp1 = data_and_dims.first[i];
+    auto currentdims = data_and_dims.second[i];
+    file.open(data_share_file_path, std::ios_base::out);
+    file << currentdims[0] << " " << currentdims[1] << "\n";
+    std::cout << "rows:" << currentdims[0] << " columns:" << currentdims[1] << std::endl;
+    for (int j = 0; j < input_values_dp1.size(); j++) {
+      file << input_values_dp1[j].Delta << " " << input_values_dp1[j].delta << "\n";
+    }
+    file.close();
+  }
+}
+
+
+void get_confirmation(const Options& options) {
+  boost::asio::io_service io_service;
+
+  // listen for new connection
+  tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), options.port));
+
+  // socket creation
+  tcp::socket socket_(io_service);
+
+  // waiting for the connection
+  acceptor_.accept(socket_);
+
+  // Read and write the number of fractional bits
+  boost::system::error_code ec;
+  int validation_bit;
+  read(socket_, boost::asio::buffer(&validation_bit, sizeof(validation_bit)), ec);
+  if (ec) {
+    std::cout << ec << "\n";
+  } else {
+    std::cout << "No Error, have received validation bit\n";
+  }
+}
+
+int main(int argc, char* argv[]) {
+  auto options = parse_program_options(argc, argv);
+  if (!options.has_value()) {
+    return EXIT_FAILURE;
+  }
+
+  retrieve_shares(*options);
+
+  if (options->my_id == 0) {
+    get_confirmation(*options);
+  }
+
+  try {
+    auto logger = std::make_shared<MOTION::Logger>(options->my_id,
+                                                   boost::log::trivial::severity_level::trace);
+
+  } catch (std::runtime_error& e) {
+    std::cerr << "ERROR OCCURRED: " << e.what() << "\n";
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
