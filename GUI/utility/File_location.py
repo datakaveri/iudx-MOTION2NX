8c30ce58ac861a994229e41d2091555d3a846888
Daksh-Prajapati <daksh.prajapathi@datakaveri.org>
2023-06-21 13:31:33 +0530

    chnged location for main.py and File_location input file path

--- GUI/utility/File_location.py
+++ GUI/utility/File_location.py
@@ -76,11 +76,11 @@ def call(fileLocation):
                 base_dir = os.getenv("BASE_DIR")
                 # subprocess.call("chmod u+r+x " + file_path_list[-1], shell=True)
                 # print("cp " + filepath +" /home/daksh1115/iudx-MOTION2NX-public/data/ImageProvider/raw_images")
-                subprocess.call("cp -r " + filepath +" " +base_dir+"/data/ImageProvider/raw_images/1111.png" , shell=True)
+                subprocess.call("cp -r " + filepath +" " +base_dir+"/data/ImageProvider/raw_images/2.png" , shell=True)
 
                 # subprocess.call("")
                 # print(file_path_list[-1])
-                loading.call("1111.png")
+                loading.call("2.png")
             except:
                 call(fileLocation)
 
